a300613178b8c9bde77788f27a3a83f3fa650a9a
Emilli Rodrigues <emillijogo2019@gmail.com>
2022-09-08 21:43:05 -0300

    aula 2

---
+++ assets/js/products.js
@@ -0,0 +1,171 @@
+async function getProducts() {
+  return [
+    {
+      id: 1,
+      name: 'Celular',
+      original_price: 6500,
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/telefone-1.jpg',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 2,
+      name: 'Caquito Dançante',
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/caquito.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 3,
+      name: 'Controle',
+      original_price: 6500,
+      is_free_delivery: true,
+      image_url: '/assets/images/controle.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 4,
+      name: 'CPU',
+      original_price: 6500,
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/cpu.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 5,
+      name: 'Flor',
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/flor.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 6,
+      name: 'Impressora',
+      original_price: 6500,
+      is_free_delivery: true,
+      image_url: '/assets/images/impessora.png',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 7,
+      name: 'Mause',
+      original_price: 6500,
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/mause.webp',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 8,
+      name: 'Notebbok',
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/notebook.jpg',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 9,
+      name: 'Teclado',
+      original_price: 6500,
+      is_free_delivery: true,
+      image_url: '/assets/images/teclado.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 10,
+      name: 'Celular 2',
+      original_price: 6500,
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/telefone-2.png',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 11,
+      name: 'Tv',
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/tv.jpg',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 12,
+      name: 'Xbox 360',
+      original_price: 6500,
+      is_free_delivery: true,
+      image_url: '/assets/images/xbox-360.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 13,
+      name: 'Batedeira',
+      original_price: 6500,
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/batedeira.jpg',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 14,
+      name: 'Jogo de Panela',
+      original_price: 6500,
+      is_free_delivery: true,
+      image_url: '/assets/images/jogo_de_panela.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 15,
+      name: 'Jogo de Talheres',
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/jogo_de_talheres.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 16,
+      name: 'Escorredor de Louça',
+      original_price: 6500,
+      is_free_delivery: true,
+      image_url: '/assets/images/escorredor_de_louça.jpg',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 17,
+      name: 'Porta Espeto',
+      original_price: 6500,
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/porta_espeto.jpg',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 18,
+      name: 'Fone de Ouvido',
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/fone_de_ouvido.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 19,
+      name: 'Árvore de Natal',
+      original_price: 6500,
+      is_free_delivery: true,
+      image_url: '/assets/images/arvore-de-natal.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    },
+    {
+      id: 20,
+      name: 'Messa',
+      original_price: 6500,
+      price: 4500,
+      is_free_delivery: true,
+      image_url: '/assets/images/messa.jfif',
+      description:  'É um fato conhecido de todos que um leitor se distrairá com...'
+    }
+  ]
+},
