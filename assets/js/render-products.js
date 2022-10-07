58f2705b0d7b7989a80ecff20448ed111ade3a45
Emilli Rodrigues <emillijogo2019@gmail.com>
2022-10-06 21:38:48 -0300

    aula 5

--- assets/js/render-products.js
+++ assets/js/render-products.js
@@ -4,21 +4,24 @@
 
 class RenderProducts {
   constructor() {
+    this.events = {}
     this.target = document.getElementById('products-list')
   }
 
   render(products) {
     const elements = products.map(product => {
       const element = createElement('div', {
-        class: 'p-4 bg-white rounded-md houver:shadow-x1 cursor-pointer',
+        class:
+          'p-4 bg-white rounded-md houver:shadow-x1 relative overflow-hidden product-item',
       })
 
       const figure = createElement(
         'figure',
-        null,
+        { class: 'flex justify-center' },
         createElement('img', {
           src: product.image_url,
           alt: product.name,
+          class: 'h-full max-h-48'
         })
       )
 
@@ -42,22 +45,22 @@ class RenderProducts {
             )
           )
 
-const freeDelivery = !product.is_free_delivery
-? ''
-: createElement(
-  'div',
-  null,
-  createElement(
-    'span',
-    { class: 'text-sm text-green-600'},
-    'Entrega grátis',
-    createElement (
-      'span',
-      { class: 'ml-2' },
-      createElement('i', { class: 'fa-solid fa-truck-fest'})
-    )
-  )
-)
+      const freeDelivery = !product.is_free_delivery
+        ? ''
+        : createElement(
+            'div',
+            null,
+            createElement(
+              'span',
+              { class: 'text-sm text-green-600' },
+              'Entrega grátis',
+              createElement(
+                'span',
+                { class: 'ml-2' },
+                createElement('i', { class: 'fa-solid fa-truck-fest' })
+              )
+            )
+          )
 
       const content = createElement(
         'div',
@@ -85,14 +88,39 @@ const freeDelivery = !product.is_free_delivery
       )
       element.append(footer)
 
-element.onclick = () => {
-  this.events.onclick(product)
-}
+      const buttonAdd = createElement(
+        'button',
+        {
+          class:
+            'py-2 flex-1 flex items-center justify-center bg-indigo-700 ' +
+            'text-white rounded-md shadow-md font-bold cursor-pointer ' +
+            'hover:bg-indigo-900',
+        },
+        'Adicionar ao carrinho'
+      )
+      const buttonWrapper = createElement(
+        'div',
+        {
+          class:
+            'p-4 flex bg-white bottom-0 left-0 right-0 absolute button-wrapper',
+        },
+        buttonAdd
+      )
+
+      buttonWrapper.onclick = () => {
+        this.events.onclick(product)
+      }
+
+      element.append(buttonWrapper)
 
       return element
     })
 
     this.target.innerHTML = ''
     this.target.append(...elements)
+  }
+
+  on(event, fn) {
+    this.events[`on${event}`] = fn
   }
 }
