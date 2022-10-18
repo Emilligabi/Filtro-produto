1ab75b18c4bc72e02962612ae8bddc43c1cd20ff
Emilli Rodrigues <emillijogo2019@gmail.com>
2022-10-17 21:32:47 -0300

    aula 10

---
+++ assets/js/events.js
@@ -0,0 +1,14 @@
+class Events {
+  constructor() {
+    this.events = {}
+  }
+
+  on(eventName, fn) {
+    this.events[eventName] = fn
+  }
+
+  emit() {
+    const [eventName, ...values] = Array.from(arguments)
+    this.events[eventName].apply(this, values)
+  }
+}
