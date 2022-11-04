3e14e766045502086e59b76c5fbc186c1078af15
Emilli Rodrigues <emillijogo2019@gmail.com>
2022-11-03 21:20:53 -0300

    penultima aula

--- assets/js/cart-list-modal.js
+++ assets/js/cart-list-modal.js
@@ -1,51 +1,118 @@
-class CartListModal {
+class CartListModal extends Events {
   constructor() {
-    this.reference = createElement('div' , {
-      classNamw: 'w-64 p-4 bg-white rounded-med shadow-md grid gap-4',
+    super()
+    this.reference = createElement('div', {
+      class:
+        'w-64 p-4 bg-slate-200 border rounded-med shadow-md gap-4 ' +
+        'hidden right-0 top-0 absolute z-10',
     })
+
+    this.timeMouseLeave = 0
+    this.reference.onmouseleave = () => {
+      this.timeMouseLeave = setTimeout(() => {
+        this.hide()
+      }, 1500)
+    }
+
+    this.reference.onmouseenter = () => {
+      clearTimeout(this.timeMouseLeave)
+    }
   }
 
-  show(product) {
+  getElement() {
+    return this.reference
+  }
+
+  render(products) {
     const items = Object.entries(
-      products.reduce((p, n) => ({ ...p, [n.id]: n }), {})
+      products.reduce((p, n) => {
+        if (!p[n.id]) {
+          p[n.id] = n
+          p[n.id].amount = 0
+        }
+
+        p[n.id].amount += 1
+
+        return p
+      }, {})
     )
-    .map(([_, product]) => product)
-    .map(product => {
-      const card = createElement(
-'li' ,
-{
-  className:
-  'bg-white rounded-md border flex items-center cart-list-item',
-},
-createElement(
-  'figure' ,
-  { className: 'p-2 flex items-center' },
-  createElement('img', {
-    className: 'w-8 h-8 rounded-full shadow-sm',
-    src: product.image_url,
-    alt: product.name,
-    title: product.name,
-  })
-),
-createElement(
-  'div',
-  { className: 'flex-1 flex flex-col' },
-  createElement('strong', {}, product.name),
-  createElement('span', {}, product.price)
-),
-createElement('div', {
-  className: 'flex items-center justify-center p-2',
-})
-      )
+      .map(([_, product]) => product)
+      .map(product => {
+        const card = createElement(
+          'li',
+          {
+            class: 'bg-white rounded-md border flex items-center relative',
+          },
+          createElement(
+            'figure',
+            { class: 'p-2 flex items-center' },
+            createElement('img', {
+              class: 'w-8 h-8 rounded-full shadow-sm',
+              src: product.image_url,
+              alt: product.name,
+              title: product.name,
+            })
+          ),
+          createElement(
+            'div',
+            { class: 'flex-1 flex flex-col items-end' },
+            createElement(
+              'strong',
+              { class: 'text-xs text-right' },
+              product.name
+            ),
+            createElement(
+              'div',
+              {},
+              createElement(
+                'span',
+                { class: 'text-xs px-2' },
+                `${product.amount}x`
+              ),
+              createElement(
+                'span',
+                { class: 'text-xs text-green-600' },
+                format(product.price)
+              )
+            )
+          ),
+          createElement('div', {
+            class: 'flex items-center justify-center p-2',
+          }),
+          createElement(
+            'button',
+            {
+              class:
+                'w-4 h-4 bg-red-600 flex  justify-center items-center ' +
+                'rounded-full shadow text-xs text-white ' +
+                'right-0 top-0 absolute',
+              onDblclick: () => this.emit('remove', product),
+            },
+            createElement('i', { class: 'fa fa-times' })
+          )
+        )
 
-      return card
-    })
+        return card
+      })
     this.reference.innerHTML = ''
+    this.reference.append(
+      ...items,
+      createElement(
+        'footer',
+        {  class: 'pt-2 mt-2 border-t border-white flex justify-between' },
+        createElement('span' , {class: 'text-xs' }, 'Total'),
+        createElement('strong', {}, format(0))
+      )
+    )
+  }
 
-    this.reference.CartList.add('active')
+  show(products) {
+    this.reference.classList.remove('hidden')
+    this.reference.classList.add('grid')
   }
 
   hide() {
-    this.reference.classList.remove('active')
+    this.reference.classList.remove('grid')
+    this.reference.classList.add('hidden')
   }
 }
--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,33 +1,40 @@
 class Cart extends Events {
-constructor() {
-  super()
+  constructor() {
+    super()
 
-  this.products = []
-  this.badge = document.getElementById('badge')
-  this.reference = document.getElementById('button-open-cart')
-  this.cartListModal = new CartListModal
+    this.products = []
+    this.badge = document.getElementById('badge')
+    this.reference = document.getElementById('button-open-cart')
+    this.cartListModal = new CartListModal()
 
-  this.reference.onclick = () => {
-this.cartListModal.show(this.products)
+    this.reference.onclick = () => {
+      this.cartListModal.show()
+    }
+
+    this.reference.ondblclick = () => {
+      window.location.href = 'cart.html'
+    }
+
+    this.cartListModal.on('remove' , product => {
+      this.removeProduct(product)
+    })
+
+    this.reference.parentElement.append(this.cartListModal.getElement())
+  }
+  addProduct(product) {
+    this.products.push(product)
+    this.updateBadge()
   }
 
-  this.reference.ondblclick = () => {
-    window.location.href = 'cart.html'
+  removeProduct(product) {
+    const index = this.products.findIndex(_ => _.id === product.id)
+    this.products.splice(index, 1)
+    this.updateBadge()
   }
-}
-addProduct(product) {
-  this.products.push(product)
-  this.updateBadge()
-}
 
-removeProduct(product) {
-  const index = this.products.findIndex(_ => _.id === product.id)
-  this.products.splice(index, 1)
-  this.updateBadge()
-}
-
-updateBadge() {
-  this.badge.innerHTML = this.products.length
-  this.badge.hidden = this.products.length === 0
-}
+  updateBadge() {
+    this.badge.innerHTML = this.products.length
+    this.badge.hidden = this.products.length === 0
+    this.cartListModal.render(this.products)
+  }
 }
